ce625512a989764cd4e5a57c51a94889affd77b2
saireddythfc <saivivaswanthreddy@gmail.com>
2025-02-04 02:24:52 +0530

    type

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
+    valid = ["exit", "echo", "type"]
     while True:
         sys.stdout.write("$ ")
 
@@ -13,6 +14,11 @@ def main():
             break
         elif command[:5] == "echo ":
             print(command[5:])
+        elif command[:5] == "type ":
+            if command[5:] in valid:
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: command not found")
         else:
             print(f"{command}: command not found")
 
