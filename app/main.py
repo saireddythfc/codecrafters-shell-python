f992aa867b3aa6395738bea966bc7f993c797fb0
saireddythfc <saivivaswanthreddy@gmail.com>
2025-02-17 18:57:41 +0530

    program run arg o/p

--- app/main.py
+++ app/main.py
@@ -46,20 +46,22 @@ def main():
         else:
             # sys.stdout.write(f"{command}: command not found" + "\n")
             exe = command.split()[0]
-            check = shutil.which(exe)
-            if check and os.access(check, os.X_OK):
-                args = command.split()
-                sys.stdout.write(
-                    f"Program was passed {len(args)} args (including program name)."
-                    + "\n"
-                )
-                sys.stdout.write(f"Arg #0 (program name): {args[0]}" + "\n")
-                for i in range(len(args) - 1):
-                    sys.stdout.write(f"Arg #{i+1}: {args[i+1]}" + "\n")
-                random_number = random.randint(10**9, 10**10 - 1)
-                sys.stdout.write(f"Program Signature: {random_number}" + "\n")
-            else:
-                sys.stdout.write(f"{command}: command not found")
+            # check = shutil.which(exe)
+            # if check and os.access(check, os.X_OK):
+            #     args = command.split()
+            #     sys.stdout.write(
+            #         f"Program was passed {len(args)} args (including program name)."
+            #         + "\n"
+            #     )
+            #     sys.stdout.write(f"Arg #0 (program name): {args[0]}" + "\n")
+            #     for i in range(len(args) - 1):
+            #         sys.stdout.write(f"Arg #{i+1}: {args[i+1]}" + "\n")
+            #     random_number = random.randint(10**9, 10**10 - 1)
+            #     sys.stdout.write(f"Program Signature: {random_number}" + "\n")
+            # else:
+            #     sys.stdout.write(f"{command}: command not found")
+
+            os.system(exe)
 
         sys.stdout.flush()
 
