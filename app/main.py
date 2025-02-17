fd3721e08e6fac0cfd58a27f54b14d406326cab7
saireddythfc <saivivaswanthreddy@gmail.com>
2025-02-17 19:25:56 +0530

    pwd format

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ def main():
     # dir_path = path[idx + 1 :] + "/"
 
     while True:
+        time.sleep(5)
         PATH = os.environ.get("PATH")
         sys.stdout.write("$ ")
         sys.stdout.flush()
