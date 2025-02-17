557b98d6544ecb490f94f4b7659802fc236f1149
saireddythfc <saivivaswanthreddy@gmail.com>
2025-02-18 03:46:04 +0530

    cd initial test working dir

--- app/main.py
+++ app/main.py
@@ -52,7 +52,8 @@ def main():
             sys.stdout.write(f"{working_dir}" + "\n")
 
         elif cmds[0] == "cd":
-            if os.path.isdir(cmds[1]):
+            print(cmds[1])
+            if os.path.exists(cmds[1]):
                 working_dir = cmds[1]
                 continue
             else:
