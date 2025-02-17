5fb6abf81852eca5c64ae674142d9329868fe9cf
saireddythfc <saivivaswanthreddy@gmail.com>
2025-02-18 04:47:49 +0530

    cd initial test working dir

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ def main():
 
         elif cmds[0] == "type":
             PATH = os.getenv("PATH")
+            print(PATH)
             word = command[5:].split()[0]
 
             paths = PATH.split(":")
